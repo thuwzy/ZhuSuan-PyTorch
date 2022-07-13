637eb1926c05f2df5674104a6c519e51a2f407f1
项晨东 <2019011831@secoder.net>
2022-07-13 15:49:35 +0800

    fix little bug

--- examples/variational_autoencoder/iwae.py
+++ examples/variational_autoencoder/iwae.py
@@ -1,6 +1,9 @@
+import math
+import os
+import sys
+
 import torch
 import torch.nn as nn
-import math, os, sys
 
 sys.path.append("..")
 from zhusuan.framework.bn import BayesianNet
@@ -31,7 +34,35 @@ class Generator(BayesianNet):
         self._obseved dict
         """
         self.observe(observed)
-        raise NotImplementedError()
+
+        try:
+            batch_len = self.observed['z'].shape[1]
+        except:
+            batch_len = 64
+        mean = torch.zeros([batch_len, self.z_dim])
+        std = torch.ones([batch_len, self.z_dim])
+
+        # the prior of z is sampled from standard normal distribution
+        z = self.sn("Normal",
+                    name="z",
+                    mean=mean,
+                    std=std,
+                    reparameterize=False,
+                    n_samples=self.n_samples,
+                    reduce_mean_dims=None,
+                    reduce_sum_dims=[2]
+                    )
+        x_probs = self.gen_sq(z)
+        self.cache["x_mean"] = x_probs
+
+        x_sample = self.sn(
+            'Bernoulli',
+            name='x',
+            probs=x_probs,
+            reduce_mean_dims=None,
+            reduce_sum_dims=[2]
+        )
+        return self
 
 
 class Variational(BayesianNet):
@@ -60,6 +91,7 @@ class Variational(BayesianNet):
         z_std = torch.exp(z_logstd)
         # get samples from StochasticTensor, z is not observed, so self.sn return new samples
         # z.shape [n_samples, batch_shape, z_dim]
+        # the z sample will be stored to self._nodes["z"]._dist.sample_cache
         z = self.sn("Normal",
                     name="z",
                     mean=z_mean,
@@ -70,3 +102,69 @@ class Variational(BayesianNet):
                     reduce_sum_dims=[2]
                     )
         return self
+
+
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    epoch_size = 10
+    batch_size = 64
+
+    z_dim = 40
+    x_dim = 28 * 28 * 1
+
+    lr = 0.001
+
+    lb_samples = 50
+
+    generator = Generator(x_dim, z_dim, lb_samples)
+    variational = Variational(x_dim, z_dim, lb_samples)
+    model = ImportanceWeightedObjective(generator, variational, axis=0, estimator="vimco").to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+
+    x_train, t_train, x_valid, t_valid, x_test, t_test = load_mnist_realval()
+
+    x_train = torch.as_tensor(x_train).to(device)
+    x_test = torch.as_tensor(x_test).to(device)
+
+    len_ = x_train.shape[0]
+    num_batches = math.ceil(len_ / batch_size)
+
+    for epoch in range(epoch_size):
+        for step in range(num_batches):
+            x = x_train[step * batch_size:min((step + 1) * batch_size, len_)]
+            x = torch.reshape(x, [-1, x_dim])
+            if x.shape[0] != batch_size:
+                break
+            loss = model({'x': x})
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (step + 1) % 100 == 0:
+                print("Epoch[{}/{}], Step [{}/{}], Loss: {:.4f}".format(epoch + 1, epoch_size, step + 1, num_batches,
+                                                                        loss.clone().cpu().detach().numpy()))
+                # float(loss.clone().detach().numpy())))
+
+    batch_x = x_test[0:64]
+    nodes_q = variational({'x': batch_x}).nodes
+    z = nodes_q['z'].tensor
+    cache = generator({'z': z}).cache
+    sample = cache['x_mean'][0].cpu().detach().numpy()
+
+    cache = generator({}).cache
+    sample_gen = cache['x_mean'][0].cpu().detach().numpy()
+
+    result_fold = './result'
+    if not os.path.exists(result_fold):
+        os.mkdir(result_fold)
+
+    batch_x = batch_x.cpu().detach().numpy()
+
+    save_img(batch_x, os.path.join(result_fold, 'iw_origin_x_.png'))
+    save_img(sample, os.path.join(result_fold, 'iw_reconstruct_x_.png'))
+    save_img(sample_gen, os.path.join(result_fold, 'iw_sample_x_.png'))
+
+
+if __name__ == '__main__':
+    main()
