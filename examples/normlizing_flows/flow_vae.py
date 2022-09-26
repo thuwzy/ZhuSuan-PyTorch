18681ae384cd44c7e575725c02ab66acf2c2aa43
Xiang-cd <xcd19@mails.tsinghua.edu.cn>
2022-09-26 19:57:17 +0800

    flow vae seems done, but need to modify elbo

--- examples/normlizing_flows/flow_vae.py
+++ examples/normlizing_flows/flow_vae.py
@@ -1,4 +1,4 @@
-import os
+import os, math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -93,16 +93,15 @@ class NICEFlow(nn.Module):
             in_out_dim=z_dim,
             mid_dim=mid_dim,
             hidden=num_hidden,
-            mask=masks[i]
+            mask=masks[i].to(device)
         ) for i in range(num_coupling)]
         flows.append(Scaling(z_dim))
         self.flow = RevSequential(flows)
 
-    def forward(self,z, **kwargs):
+    def forward(self, z, **kwargs):
         out, log_det_J = self.flow.forward(z, **kwargs)
         res = {"z": out}
         return res, log_det_J
-
 
 
 class HF(nn.Module):
@@ -112,15 +111,144 @@ class HF(nn.Module):
             self.v_layer = nn.Linear(v_dim, z_dim)
         else:
             self.v_layer = nn.Linear(z_dim, z_dim)
+
     def forward(self, z, v, **kwargs):
         v_new = self.v_layer(v)
         vvT = torch.bmm(v_new.unsqueeze(2), v_new.unsqueeze(1))
         vvTz = torch.bmm(vvT, z.unsqueeze(2)).squeeze(2)
         norm_sq = torch.sum(v_new * v_new, dim=1, keepdim=True)
-        #TODO
+        norm_sq = norm_sq.expand(norm_sq.shape[0], v_new.shape[1])
+        z_new = z - 2 * vvTz / norm_sq
+        return (z_new, v_new), torch.zeros([1, 1])
+
 
 class HouseHolderFlow(nn.Module):
     def __init__(self, z_dim, v_dim, n_flows):
         super(HouseHolderFlow, self).__init__()
+        _first_kwarg = {'is_first': True, 'v_dim': v_dim}
+        self.flow = nn.Sequential(*[HF(z_dim, **_first_kwarg) if _ == 0 else HF(z_dim) for _ in range(n_flows)])
+
+    def forward(self, z, v, **kwargs):
+        out, log_det = self.flow.forward(z, v, **kwargs)
+        res = {"z": out[0]}
+        return res, log_det
+    
+
+class PF(nn.Module):
+    def __init__(self, z_dim):
+        super(PF, self).__init__()
+        self.u = self.create_parameter(shape=[1, z_dim])
+        self.w = self.create_parameter(shape=[1, z_dim])
+        self.b = self.create_parameter(shape=[1])
+        self.add_parameter('u', self.u)
+        self.add_parameter('w', self.w)
+        self.add_parameter('b', self.b)
+
+    def forward(self, z, **kwargs):
+        def m(z):
+            return F.softplus(z) - 1.
+
+        def h(z):
+            return torch.tanh(z)
+
+        def h_prime(z):
+            return 1. - torch.square(h(z))
+
+        inner = torch.sum(self.w * self.u)
+        u = self.u + (m(inner) - inner) * self.w / torch.square(self.w.norm())
+        activation = torch.sum(self.w * z, dim=1, keepdim=True) + self.b
+        z_new = z + u * h(activation)
+        psi = h_prime(activation) * self.w
+        log_det = torch.log(torch.abs(1. + torch.sum(u * psi, dim=1, keepdim=True)))
+        return (z_new,), log_det
 
 
+class PlanarFlow(nn.Module):
+    def __init__(self, z_dim, n_flows):
+        super(PlanarFlow, self).__init__()
+        self.flows = nn.Sequential(*[PF(z_dim) for _ in range(n_flows)])
+
+    def forward(self, z, **kwargs):
+        out, log_det = self.flows.forward(z, **kwargs)
+        res = {'z': out[0]}
+        return res, log_det
+
+
+
+def main():
+    # Define model parameters
+    epoch_size = 10
+    batch_size = 64
+
+    z_dim = 40
+    x_dim = 28 * 28 * 1
+
+    mid_dim_flow = 64
+    num_coupling = 10
+    num_hidden_per_coupling = 4
+
+    lr = 0.001
+
+    # create the network
+    generator = Generator(x_dim, z_dim, batch_size)
+    variational = Variational(x_dim, z_dim, batch_size)
+    nice_flow = NICEFlow(z_dim, mid_dim_flow, num_coupling, num_hidden_per_coupling)
+    model = ELBO(generator, variational, transform=nice_flow, transform_var=['z'])
+    # planar_flow = PlanarFlow(z_dim, 1)
+    # model = ELBO(generator, variational, transform=planar_flow, transform_var=['z'])
+    # householder_flow = HouseHolderFlow(z_dim, 500, 5)
+    # model = ELBO(generator, variational, transform=householder_flow, transform_var=['z'], auxillary_var=['z_logits'])
+    # model = ELBO(generator, variational)
+
+    clip = fluid.clip.GradientClipByNorm(clip_norm=1.0)
+    optimizer = torch.optimizer.Adam(learning_rate=lr,
+                                      parameters=model.parameters(), )
+
+    x_train, t_train, x_valid, t_valid, x_test, t_test = load_mnist_realval()
+
+    # do train
+    len_ = x_train.shape[0]
+    num_batches = math.ceil(len_ / batch_size)
+
+    for epoch in range(epoch_size):
+        for step in range(num_batches):
+            x = torch.as_tensor(x_train[step * batch_size:min((step + 1) * batch_size, len_)])
+            x = torch.reshape(x, [-1, x_dim])
+            if x.shape[0] != batch_size:
+                continue
+
+            ##loss= model(x)
+            loss = model({'x': x})
+            assert (generator.log_joint().shape == [])
+
+            loss.backward()
+            optimizer.step()
+            optimizer.clear_grad()
+
+            if (step + 1) % 100 == 0:
+                print("Epoch[{}/{}], Step [{}/{}], Loss: {:.4f}"
+                      .format(epoch + 1, epoch_size, step + 1, num_batches, float(loss.numpy())))
+
+    # eval
+    batch_x = x_test[0:batch_size]
+    nodes_q = variational({'x': torch.to_tensor(batch_x)}).nodes
+    z = nodes_q['z'].tensor
+    cache = generator({'z': z}).cache
+    sample = cache['x_mean'][0].numpy()
+
+    # z = torch.to_tensor(np.load('z.npy').astype(np.float32))
+    cache = generator({'z': z}).cache
+    sample_gen = cache['x_mean'][0].numpy()
+
+    result_fold = './results/flow-VAE'
+    if not os.path.exists(result_fold):
+        os.mkdir(result_fold)
+
+    print([sample.shape, batch_x.shape])
+    save_img(batch_x, os.path.join(result_fold, 'origin_x_VAE+NICE.png'))
+    save_img(sample, os.path.join(result_fold, 'reconstruct_x_VAE+NICE.png'))
+    save_img(sample_gen, os.path.join(result_fold, 'sample_x_VAE+NICE.png'))
+
+
+if __name__ == '__main__':
+    main()
